5ab0db315e31450a70c2ea441b9bb56bddc59d79
Nishant <nishantsingh95340@gmail.com>
2023-06-08 16:09:01 +0530

    addin protexctyded routes

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+<em></em>;
+import "./App.css";
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+import Profile from "./component/Profile";
+import About from "./component/About";
+import Protected from "./component/Protected";
+
+function App() {
+  const [isLoggedIn, setisLoggedIn] = useState(null);
+  const logIn = () => {
+    setisLoggedIn(true);
+  };
+  const logOut = () => {
+    setisLoggedIn(false);
+  };
+  return (
+    <>
+      <div>
+        <BrowserRouter>
+          <Navbar />
+          {isLoggedIn ? (
+            <button onClick={logOut}>Logout</button>
+          ) : (
+            <button onClick={logIn}>Login</button>
+          )}
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>404 page Not Found</p>
+                </main>
+              }
+            ></Route>
+            <Route
+              path="/profile"
+              element={
+                <Protected isLoggedIn={isLoggedIn}>
+                  <Profile />
+                </Protected>
+              }
+            ></Route>
+            <Route
+              path="/about"
+              element={
+                <Protected isLoggedIn={isLoggedIn}>
+                  <About />
+                </Protected>
+              }
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav style={{ textAlign: "center", marginTop: "20px" }}>
+      <Link to="/" style={{ padding: "10px" }}>
+        Home
+      </Link>
+      <Link to="/profile" style={{ padding: "10px" }}>
+        Profile
+      </Link>
+      <Link to="/about" style={{ padding: "10px" }}>
+        About
+      </Link>
+    </nav>
+  );
+};
+export default Navbar;
